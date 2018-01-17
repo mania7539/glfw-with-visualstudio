7d2bbb15a1d710c91fc979ebfc8a4a99744f7e86
David.Rz.Liu <mania7539@gmail.com>
2018-01-17 23:57:59 +0800

    l8: How I Deal with Shaders in OpenGL

--- glfw-gl3/src/main.cpp
+++ glfw-gl3/src/main.cpp
@@ -1,6 +1,50 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSources {
+	std::string vertexSource;
+	std::string fragmentSource;
+};
+
+static ShaderProgramSources parseShader(const std::string& filepath) {
+
+	enum class ShaderType {
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+	std::cout << filepath << std::endl;
+	std::ifstream stream(filepath);
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+
+	while (getline(stream, line)) {
+		// npos is basically an invalid string position
+		if (line.find("#shader") != std::string::npos) {
+			if (line.find("vertex") != std::string::npos) {
+				// set mode to vertex
+				type = ShaderType::VERTEX;
+
+			}
+			else if (line.find("fragment") != std::string::npos) {
+				// set mode to fragment
+				type = ShaderType::FRAGMENT;
+			}
+
+		}
+		else {
+			ss[(int)type] << line << '\n';
+		}
+	}
+
+	return {
+		ss[0].str(), ss[1].str()
+	};
+}
+
 
 static unsigned int compileShader(unsigned int type, const std::string& source) {
 	unsigned int id = glCreateShader(type);
@@ -14,9 +58,9 @@ static unsigned int compileShader(unsigned int type, const std::string& source) 
 	if (!result) {
 		int length;
 		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-		char* message = (char*) alloca(length * sizeof(char));
+		char* message = (char*)alloca(length * sizeof(char));
 		glGetShaderInfoLog(id, length, &length, message);
-		std::cout << "Failed to compile " << 
+		std::cout << "Failed to compile " <<
 			(type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
 		std::cout << message << std::endl;
 		glDeleteShader(id);
@@ -72,14 +116,14 @@ int main(void)
 		0.0f,  0.5f,
 		0.5f, -0.5f
 	}; // This is vertex positions of our triangle
-	// But a "vetex" doesn't only contain position, it can contain more of that
-	// : a position is an attribute, a color is an attribute, a texture coordinate is an attribute
-	// : a normal is an attribute
-	// NOTE: VertexAttribPointer - Attribute is a part which you can deal with your self or managed by opengl by default
-	// : stride - Specifies the byte offset between consecutive generic vertex attributes.
-	//				(: it's the amount of bytes between each vertex, or the size of each vertex)
-	//				you'll get weird artifacts in your rendering or just black screen while you just get only 1 byte out of place
-	
+	   // But a "vetex" doesn't only contain position, it can contain more of that
+	   // : a position is an attribute, a color is an attribute, a texture coordinate is an attribute
+	   // : a normal is an attribute
+	   // NOTE: VertexAttribPointer - Attribute is a part which you can deal with your self or managed by opengl by default
+	   // : stride - Specifies the byte offset between consecutive generic vertex attributes.
+	   //				(: it's the amount of bytes between each vertex, or the size of each vertex)
+	   //				you'll get weird artifacts in your rendering or just black screen while you just get only 1 byte out of place
+
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);				// Generate/Create a GL Buffer, we should provide an Integer as a memory which we can write into 
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);	// How do I want to use the GL Buffer? Define it to a specific buffer:
@@ -87,44 +131,53 @@ int main(void)
 											//	buffer - an integer comes from memory
 											// if we use glBindBuffer(GL_ARRAY_BUFFER, 0); then GPU won't draw the triangle out since we bind something else
 
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);	
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 	// above: Set data which we want to use to the specific GPU buffer
 	// : STATIC, DYNAMIC: we should let GPU knows that if the buffer can be modified more than ONCE.
 	// : DRAW: we want to draw things with the buffer, so use it
 
 	glEnableVertexAttribArray(0);	// index: it's the index we want to enable attribute.
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2,  0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 	// we need to tell opengl how we layout the data with - glVertexAttribPointer
 	// example: glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*) 8);
 	// : normal: GL_FALSE - if we want them to be normalized
 	// : pass 4*2 = 8 as offset (in byte) to the texture coordinate - converts to a pointer with (const void*)
 	// IMPORTANT: we need to enable attribute feature with - glEnableVertexAttribArray
 
+	// vertex shader:
 	// gl_Position is actually a "vec4" (definition I guess), 
 	//		and it's value should be as the same as in "index" argument of: glVertexAttribPointer
 	//		so when we specify layout(location = 0), it should also be vec4, 
 	//		or we need to cast it to vec4 while we assign it to gl_Position with vec4(position.xy)
 	// https://www.youtube.com/watch?v=71BLZwRGUJE&index=7&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2
 	// don't need to use + to concatenate string, since it's in c++ style
-	std::string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
+	/*std::string vertexShader =
+	"\\\\comment"
+	"#version 330 core\n"
+	"\n"
+	"layout(location = 0) in vec4 position;\n"
+	"void main()\n"
+	"{\n"
+	"	gl_Position = position;\n"
+	"}\n";*/
 
-	// assign "color" with RGBA value => Red, the layout of RGBA sequence actually depends on your framebuffer format
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-		"}\n";
-	unsigned int shader = createShader(vertexShader, fragmentShader);
+	// fragment shader: assign "color" with RGBA value => Red, the layout of RGBA sequence actually depends on your framebuffer format
+	/*std::string fragmentShader =
+	"#version 330 core\n"
+	"\n"
+	"layout(location = 0) out vec4 color;\n"
+	"void main()\n"
+	"{\n"
+	"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+	"}\n";*/
+	//unsigned int shader = createShader(vertexShader, fragmentShader);
+	//glUseProgram(shader);
+	ShaderProgramSources source = parseShader("res/shaders/basic.shader");
+	std::cout << "VERTEX: " << std::endl;
+	std::cout << source.vertexSource << std::endl;
+	std::cout << "FRAGMENT: " << std::endl;
+	std::cout << source.fragmentSource << std::endl;
+	unsigned int shader = createShader(source.vertexSource, source.fragmentSource);
 	glUseProgram(shader);
 
 	/*
@@ -143,7 +196,7 @@ int main(void)
 		// : first - 0 means the index of the array we want to start
 		// : count - the number of axis (x, y) of the array we want to draw
 		// NOTE: similar API - glDrawElements(GL_TRIANGLES, 3, UNSIGNED_INT, ...) used with index buffer
-		
+
 
 
 		/* Swap front and back buffers */
@@ -153,7 +206,7 @@ int main(void)
 		glfwPollEvents();
 	}
 
-	glDeleteShader(shader);	// delete shader after the app ends
+	//glDeleteShader(shader);	// delete shader after the app ends
 
 	glfwTerminate();
 	return 0;
